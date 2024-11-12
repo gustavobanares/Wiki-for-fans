d19f1180276613924a3cfea0822dcf9d20a3c2cd
*Gustavo <gustavobbacco@gmail.com>
2024-11-12 19:28:57 -0300

    chore: setup to project

---
+++ wifi-for-fans/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',  // Incluir os arquivos JS e JSX no src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
